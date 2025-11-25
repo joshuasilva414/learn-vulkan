54234abd23e644d282b3275ac849b90a42d0b6be
joshuasilva414 <joshuasilva414@gmail.com>
2025-11-24 19:18:24 -0600

    some stuff that works

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const glfw = @import("zglfw");
+const vk = @import("vulkan");
 
 pub fn main() !void {
     var major: i32 = 0;
@@ -12,18 +13,30 @@ pub fn main() !void {
     //Example of something that fails with GLFW_NOT_INITIALIZED - but will continue with execution
     //var monitor: ?*glfw.Monitor = glfw.getPrimaryMonitor();
 
-    try glfw.init();
+    glfw.init() catch return error.GlfwInitFailed;
     defer glfw.terminate();
     std.debug.print("GLFW Init Succeeded.\n", .{});
 
-    const window: *glfw.Window = try glfw.createWindow(800, 640, "Hello World", null, null);
+    if (glfw.vulkanSupported() != true) {
+        std.log.err("GLFW could not find libvulkan", .{});
+        return error.NoVulkan;
+    }
+
+    const window: *glfw.Window = glfw.createWindow(800, 640, "Hello World", null, null) catch return error.WindowInitFailed;
     defer glfw.destroyWindow(window);
 
-    while (!glfw.windowShouldClose(window)) {
-        if (glfw.getKey(window, glfw.KeyEscape) == glfw.Press) {
-            glfw.setWindowShouldClose(window, true);
-        }
+    glfw.setWindowAttrib(window, glfw.ClientAPI, glfw.NoAPI);
+    glfw.setWindowAttrib(window, glfw.Resizable, 0);
 
-        glfw.pollEvents();
-    }
+    // var app = vk.VulkanApp.init(window);
+    // defer app.denit();
+
+    // try app.run();
+    // while (!glfw.windowShouldClose(window)) {
+    //     if (glfw.getKey(window, glfw.KeyEscape) == glfw.Press) {
+    //         glfw.setWindowShouldClose(window, true);
+    //     }
+
+    //     glfw.pollEvents();
+    // }
 }
