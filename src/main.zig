3ffcdc52bdbcc5ac3bc2286e4e9a3cbc586e8568
joshuasilva414 <joshuasilva414@gmail.com>
2026-01-11 22:25:32 -0600

    window opens

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const glfw = @import("zglfw");
 const vk = @import("vulkan");
+const GraphicsContext = @import("graphics_context.zig").GraphicsContext;
+const print = std.debug.print;
 
 pub fn main() !void {
     var major: i32 = 0;
@@ -8,35 +10,40 @@ pub fn main() !void {
     var rev: i32 = 0;
 
     glfw.getVersion(&major, &minor, &rev);
-    std.debug.print("GLFW {}.{}.{}\n", .{ major, minor, rev });
+    print("GLFW {}.{}.{}\n", .{ major, minor, rev });
 
     //Example of something that fails with GLFW_NOT_INITIALIZED - but will continue with execution
     //var monitor: ?*glfw.Monitor = glfw.getPrimaryMonitor();
 
     glfw.init() catch return error.GlfwInitFailed;
     defer glfw.terminate();
-    std.debug.print("GLFW Init Succeeded.\n", .{});
+    print("GLFW Init Succeeded.\n", .{});
 
-    if (glfw.vulkanSupported() != true) {
+    if (!glfw.vulkanSupported()) {
         std.log.err("GLFW could not find libvulkan", .{});
         return error.NoVulkan;
     }
 
+    glfw.windowHint(glfw.ClientAPI, glfw.NoAPI);
+    glfw.windowHint(glfw.Resizable, 1);
     const window: *glfw.Window = glfw.createWindow(800, 640, "Hello World", null, null) catch return error.WindowInitFailed;
     defer glfw.destroyWindow(window);
 
-    glfw.setWindowAttrib(window, glfw.ClientAPI, glfw.NoAPI);
-    glfw.setWindowAttrib(window, glfw.Resizable, 0);
+    const gc = try GraphicsContext.init(std.heap.c_allocator, "Vulkan App", window);
+    defer gc.deinit();
 
-    // var app = vk.VulkanApp.init(window);
-    // defer app.denit();
+    // print("{d} extensions supported\n", .{extensionCount});
 
-    // try app.run();
     // while (!glfw.windowShouldClose(window)) {
-    //     if (glfw.getKey(window, glfw.KeyEscape) == glfw.Press) {
-    //         glfw.setWindowShouldClose(window, true);
-    //     }
-
     //     glfw.pollEvents();
     // }
+
+    // try app.run();
+    while (!glfw.windowShouldClose(window)) {
+        if (glfw.getKey(window, glfw.KeyEscape) == glfw.Press) {
+            glfw.setWindowShouldClose(window, true);
+        }
+
+        glfw.pollEvents();
+    }
 }
